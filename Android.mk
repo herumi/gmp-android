c55be422ad6b87c45174a1fab73a864a5e87dafa
MITSUNARI Shigeo <herumi@nifty.com>
2017-02-08 14:38:30 +0900

    add shared library

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(GMP_LINK_STATIC,1)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := gmp
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).a
@@ -9,3 +11,17 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := gmpxx
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).a
 include $(PREBUILT_STATIC_LIBRARY)
+
+else
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := gmp
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := gmpxx
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).so
+include $(PREBUILT_SHARED_LIBRARY)
+
+endif
